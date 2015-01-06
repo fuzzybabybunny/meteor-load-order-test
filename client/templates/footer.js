babd713dfe81c374abb6bef331ac449e5c0d2095
fuzzybabybunny <victor@victorlinphoto.com>
2015-01-05 19:35:13 -0500

    added text

--- client/templates/footer.js
+++ client/templates/footer.js
@@ -1,5 +1,6 @@
 Template.Footer.rendered = function(){
 
 	console.log('Footer template rendered and the number of DOM nodes is ' + document.getElementsByTagName('*').length);
+	$('#partial1Value').text('This text was added using jQuery inside of Template.Footer.rendered() and the DOM node that was selected was originally created when Partial 1 was rendered');
 
 };
