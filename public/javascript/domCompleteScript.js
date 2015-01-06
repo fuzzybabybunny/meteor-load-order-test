6bd383b1aeaf14929e0c097b67e0556de9cec688
fuzzybabybunny <victor@victorlinphoto.com>
2015-01-06 15:04:19 -0500

    added document.ready after meteor.startup

--- public/javascript/domCompleteScript.js
+++ public/javascript/domCompleteScript.js
@@ -1,1 +1,5 @@
-console.log('Loaded from Layout.rendered callback. This should only fire after all DOM nodes have been loaded.');
+console.log('a script in /public/javascript referenced in the Layout.rendered callback. This should only fire after all DOM nodes have been loaded.');
+
+$('document').ready(function(){
+	console.log('$("document").ready(func) is firing from a script in /public/javascript referenced in the Layout.rendered callback. All DOM nodes should be present at this time.');
+});
